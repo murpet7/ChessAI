8a8026a5a56ee1d6d1a2e75937721d734af94a01
bvandenberg7 <b.vandenberg7@students.uu.nl>
2023-07-24 00:21:14 +0200

    Clock added

--- src/computerPlayer.cpp
+++ src/computerPlayer.cpp
@@ -12,7 +12,16 @@ void ComputerPlayer::NotifyTurnToMove(Move move)
 
 void ComputerPlayer::FindBestMove()
 {
+    std::clock_t c_start = std::clock();
+    auto t_start = std::chrono::high_resolution_clock::now();
     chosenMove = GetBestMove(board, 3, board.colorToMove);
+    std::clock_t c_end = std::clock();
+    auto t_end = std::chrono::high_resolution_clock::now();
+    std::cout << std::fixed << std::setprecision(2) << "CPU time used: "
+              << 1000.0 * (c_end - c_start) / CLOCKS_PER_SEC << " ms\n"
+              << "Wall clock time passed: "
+              << std::chrono::duration<double, std::milli>(t_end - t_start).count()
+              << " ms\n";
     board.MovePiece(chosenMove);
     isMoveChosen = true;
 }
